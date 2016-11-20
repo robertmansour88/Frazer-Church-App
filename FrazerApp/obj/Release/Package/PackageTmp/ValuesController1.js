76d6713bbc6ffddfcd63acf306943622491d3c25
robertmansour88 <robert.mansour88@gmail.com>
2016-11-20 15:21:53 -0800

    Initial Code

---
+++ FrazerApp/obj/Release/Package/PackageTmp/ValuesController1.js
@@ -0,0 +1,11 @@
+﻿//link script to index.html page
+//GET request endpoint at api/ValuesController1/get
+//save result in a varible
+//console.log the varibile 
+
+//alert("Welcome to My Javascript file")
+var xhr = new XMLHttpRequest();
+xhr.send();
+currentLocation = window.location.href;
+//window.alert(currentLocation);
+console.log(currentLocation);
