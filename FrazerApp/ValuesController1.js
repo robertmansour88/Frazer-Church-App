9df0dfd6068e3a0e48c6191e20c1c526227dde32
robertmansour88 <robert.mansour88@gmail.com>
2016-11-20 15:41:22 -0800

    Test

--- FrazerApp/ValuesController1.js
+++ FrazerApp/ValuesController1.js
@@ -2,10 +2,11 @@
 //GET request endpoint at api/ValuesController1/get
 //save result in a varible
 //console.log the varibile 
-
+debugger;
 //alert("Welcome to My Javascript file")
 var xhr = new XMLHttpRequest();
-xhr.send();
-currentLocation = window.location.href;
+xhr.open('GET', '/api/products');
+xhr.send('/api/products');
+//currentLocation = window.location.href;
 //window.alert(currentLocation);
-console.log(currentLocation);
+//console.log(currentLocation);
