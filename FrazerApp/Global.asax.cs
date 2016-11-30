da33df00fbf3464a827965c1e59ec790d0fbea5b
beshoy hanna <beshoyhanna@outlook.com>
2016-11-29 20:05:00 -0500

    update csproj and global.asax.cs

--- FrazerApp/Global.asax.cs
+++ FrazerApp/Global.asax.cs
@@ -11,7 +11,7 @@ namespace FrazerApp
     {
         protected void Application_Start()
         {
-            //GlobalConfiguration.Configure(WebApiConfig.Register);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
 }
