009832b725a02d4ec2ece4ca6a06dbf84532416a
robertmansour88 <robert.mansour88@gmail.com>
2017-01-28 18:55:22 -0800

    changes

--- FrazerApp/app/main.js
+++
@@ -1,7 +0,0 @@
-﻿(function (app) {
-    document.addEventListener('DOMContentLoaded', function () {
-        ng.platformBrowserDynamic
-          .platformBrowserDynamic()
-          .bootstrapModule(app.AppModule);
-    });
-})(window.app || (window.app = {}));
